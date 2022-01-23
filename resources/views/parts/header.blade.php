e36fc9c16dc2ca11e68f014d87a7f50f2e0bd4ac
hoshimotoyasunori@gmail.com <sanix.fukuoka@gmail.com>
2022-01-23 20:52:04 +0900

    first commit

---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+	<h1>test page</h1>
+	<nav>
+		<a href="/">Top</a>
+	</nav>
+</header>
---
+++ resources/views/top.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="{{  asset('css/style.css') }}" />
+    </head>
+    <body>
+
+        @include("parts.header")
+
+        <main>
+            <h1>トップページです</h1>
+        </main>
+
+        @include("parts.footer")
+
+    </body>
+</html>
